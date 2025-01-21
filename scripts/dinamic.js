3488287577d6dcb31ace981b2d8a4bff00e6e3fd
NoNiQu <marcos.workstation@gmail.com>
2025-01-21 18:28:34 +0100

    yes

--- scripts/dinamic.js
+++ scripts/dinamic.js
@@ -1,8 +1,55 @@
 // Obtener el contenedor donde se mostrará la galería
-const galleryContainer = document.getElementById("gallery"); // Asegúrate de tener un div con id="gallery" en tu HTML
+const galleryContainer = document.getElementById("gallery");
 
 // Configuración de las imágenes
-const universos = ["universo0", "universo1", "universo2", "universo3"]; // Agrega tantos nombres como universos tengas
+const universos = [
+  {
+    id: 0,
+    name: "universo0",
+    description: "Universo vacío"
+  },
+  {
+    id: 1,
+    name: "universo1",
+    description: "Universo gatuno"
+  },
+  {
+    id: 2,
+    name: "universo2",
+    description: "Universo de la nada"
+  },
+  {
+    id: 3,
+    name: "universo3",
+    description: "Universo del taco"
+  },
+  {
+    id: 4,
+    name: "universo4",
+    description: "Universo de Cerveza"
+  },
+  {
+    id: 5,
+    name: "universo5",
+    description: "Universo Eyeliner"
+  },
+  {
+    id: 6, 
+    name: "universo6",
+    description: "Universo Cuñado"
+  },
+  {
+    id: 7,
+    name: "universo7",
+    description: "Universo Indiferente"
+  },
+  {
+    id: 8,
+    name: "universo8",
+    description: "Universo Cachondo"
+  }
+];
+
 const sizes = ["small", "medium", "large", "xlarge"];
 const resolutions = ["1x", "2x"];
 
